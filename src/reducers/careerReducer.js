58b50670ae9a455917c1bdc49e86eb4531a209be
Viktor Ölund <viktor.olund@vll.se>
2017-06-26 14:28:05 +0200

    refactor(careerReducer): spread state and careerdata

--- src/reducers/careerReducer.js
+++ src/reducers/careerReducer.js
@@ -1,30 +1,27 @@
-import * as actionTypes from '../actions/actionTypes';
-import initialState from './initialState';
+import * as actionTypes from '../actions/actionTypes'
+import initialState from './initialState'
 
 export default function careerReducer(state = initialState.career, action) {
   switch (action.type) {
     case actionTypes.LOAD_CAREER_SUCCESS:
 
-        // return Object.assign({}, state, {
-        //     action.career.career.viktor.jobs
-        // });
-
-        //return action.career;
-
-        const careerData = [];
+        const careerData = []
 
         action.career.career.viktor.jobs.map((career) => {
-            let careerKeys = Object.keys(career)[0];
-            let careerProperties = career[careerKeys];
+            let careerKeys = Object.keys(career)[0]
+            let careerProperties = career[careerKeys]
 
-            careerProperties.baseTitle = careerKeys;
+            careerProperties.baseTitle = careerKeys
 
-            careerData.push(careerProperties);
-        });
+            careerData.push(careerProperties)
+        })
 
-        return careerData;
+        return [
+            ...state,
+            ...careerData
+        ]
 
     default:
-      return state;
+      return state
   }
 }
