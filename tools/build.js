9947ff5127c2dc6ba8d94b187a2788c0f1b3531d
Viktor Ölund <viktor.olund@vll.se>
2017-06-30 14:25:35 +0200

    style(tools): semi

--- tools/build.js
+++ tools/build.js
@@ -1,34 +1,31 @@
-// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
-// Allowing console calls below since this is a build file.
 /*eslint-disable no-console */
-import webpack from 'webpack';
-import webpackConfig from '../webpack.config';
+import webpack from 'webpack'
+import webpackConfig from '../webpack.config.prod'
 
-process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn't apply.
+process.env.NODE_ENV = 'production' // this assures the Babel dev config (for hot reloading) doesn't apply.
 
-console.log('Generating minified bundle for production via Webpack. This will take a moment...');
+console.log('Generating minified bundle for production via Webpack. This will take a moment...')
 
 webpack(webpackConfig).run((err, stats) => {
-  if (err) { // so a fatal error occurred. Stop here.
-    console.log(err);
-    return 1;
+  if (err) {
+    console.log(err)
+    return 1
   }
 
-  const jsonStats = stats.toJson();
+  const jsonStats = stats.toJson()
 
   if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(error));
+    return jsonStats.errors.map(error => console.log(error))
   }
 
   if (jsonStats.hasWarnings) {
-    console.log('Webpack generated the following warnings: ');
-    jsonStats.warnings.map(warning => console.log(warning));
+    console.log('Webpack generated the following warnings: ')
+    jsonStats.warnings.map(warning => console.log(warning))
   }
 
-  console.log(`Webpack stats: ${stats}`);
+  console.log(`Webpack stats: ${stats}`)
 
-  // if we got this far, the build succeeded.
-  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!');
+  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!')
 
-  return 0;
-});
+  return 0
+})
